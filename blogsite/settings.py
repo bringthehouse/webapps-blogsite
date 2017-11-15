2ebd2e751feb369fabbc03be38e2d8a8df9b22cb
root <root@ta2.dhcp.nd.edu>
2017-11-15 18:57:43 -0500

    Settings file updates for postgresql and truncate in list view.

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*8x0r&#0)qka6fbr_8v=(i%7g1u3hg-$ug36t7xcii0wr+^xrf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'ta2.dhcp.nd.edu']
 
 
 # Application definition
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'blog',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
